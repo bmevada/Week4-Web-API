643d41873a76c96f4c96bc81301e9eb9eeb8672e
Bhavika Mevada <bmevada>
2022-08-22 19:32:53 +1000

    added range of questions

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+// Add List of all questions, choice and answers
+var questions = [
+    {
+        tile: 'Commonly used data types DO NOT inlcude',
+        choices: ['strings', 'booleans', 'alerts', 'numbers'],
+        answer: 'alerts'.
+    }
+
+    {
+        tile: 'The condition is an "if" / "else" statement is enclosed with an ...',
+        choices: ['quotes', 'curly  brackets', 'parentheses', 'square brackets'],
+        answer: 'parentheses',
+    }
+
+    {
+        tile: '',
+        choices: [''],
+        answer: '',
+    }
+
+    {
+        tile: '',
+        choices: [''],
+        answer: '',
+    }
+
+    {
+        tile: '',
+        choices: [''],
+        answer:
+    }
+]
