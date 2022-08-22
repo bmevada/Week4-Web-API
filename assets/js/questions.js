a4e2d7c492b072e06e13a8130d9518f99be3a593
Bhavika Mevada <bmevada>
2022-08-22 20:28:54 +1000

    new html file with errors

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,26 +3,26 @@ var questions = [
     {
         tile: 'Commonly used data types DO NOT inlcude',
         choices: ['strings', 'booleans', 'alerts', 'numbers'],
-        answer: 'alerts'.
-    }
+        answer: 'alerts',
+    },
 
     {
         tile: 'The condition is an "if" / "else" statement is enclosed with an ...',
         choices: ['quotes', 'curly  brackets', 'parentheses', 'square brackets'],
         answer: 'parentheses',
-    }
+    },
 
     {
         tile: 'Arrays in JavaScripr can be used to store ...',
         choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
         answer: 'all of the above',
-    }
+    },
 
     {
         tile: 'String values must be enclosed within ... when being assigned to the variables.',
         choices: [''],
         answer: 'quotes',
-    }
+    },
 
     {
         tile: 'A very useful tool used during development and debugging for printing content to the debuggers is ...',
